45c6c16fd2f12396b79947d9bdcfa2fd40a16038
Lidia Chia <angaia@gmail.com>
2016-07-26 13:03:12 +0200

    Initial folder state

---
+++ js/custom.js
@@ -0,0 +1,89 @@
+var words = ['cacas', 'cocotero', 'fliskis']
+
+var decomMaxIndex = words.length-1;
+var decomRandomIndex = Math.floor(Math.random() * (decomMaxIndex + 1));
+var word = words[decomRandomIndex].split("");
+var wordHidden = [];
+var usedLetters = [];
+var fails = usedLetters.length + 1;
+
+//Get word
+$(document).ready(newWord());
+
+//Surrender
+$('.surrender').click(function (event) {
+	event.preventDefault()
+	lose();
+});
+
+//Submit by ENTER
+$('input').keypress( function (event){
+
+	if (event.which == '13') {
+		event.preventDefault()
+		submit();
+	};	
+});
+
+// Submit by CLICK
+$('.submit').click( function (event){
+	event.preventDefault()
+	submit();
+});
+
+// Get new
+function newWord () {
+	for (var i = 0; i < word.length; i++) {
+		wordHidden.push("-");
+	}
+	$('#word').html(wordHidden);
+}
+// Submit AND see what happens 
+function submit (){
+	var used = $('input').val();
+		usedLetters.push(used);
+		$('.used').html('<p>' + usedLetters + '</p>');
+		$('.failsNumber').html(fails++);
+		$('input').val("");
+		$('img').attr('src','hangman_smilie/smilie_hangman-0' + fails +'.png');
+
+	for (var i = 0; i < word.length; i++) {
+		//if letter exists within the word
+		if (word[i] === used) {
+			console.log ('cool');
+			wordHidden[i] = used;
+			console.log(wordHidden);
+			$('#word').html(wordHidden);
+			win();
+
+		} else {
+		//if letter DOES NOT exist within the word
+			console.log ('caca');
+		}
+	};
+	// No lives left
+	if (usedLetters.length >= 8) {
+		lose();
+	}		
+}
+
+// Victory
+function win () {
+	var compareOriginal = word.toString();
+	var compareGuess = wordHidden.toString();
+
+	if (compareOriginal === compareGuess) {	
+	$('img').attr('src','hangman_smilie/smilie_hangman-00.png');
+	$('.hangman').prepend('<h2>You win!!!</h2>');
+	}
+}
+
+// Defeat
+function lose (){
+	$('img').attr('src','hangman_smilie/smilie_hangman-09.png');
+	$('.hangman').prepend('<h2>YOU LOSE!!!</h2>');
+	$('#word').html(word);
+}
+
+
+
